4be79145dca5f48e250d2f17d483f7a9dc951b01
Anton Gor <anton-gor-dev@hotmail.com>
2026-05-30 15:06:39 +0000

    chore: initialize project foundation

---
+++ packages/core/src/runtime/registry.ts
@@ -0,0 +1,15 @@
+import { ComposableInstance } from "./types"
+
+const composables = new Map<string, ComposableInstance>()
+
+export function registerInstance(record: ComposableInstance) {
+  composables.set(record.id, record)
+}
+
+export function unregisterInstance(id: string) {
+  composables.delete(id)
+}
+
+export function getInstances() {
+  return [...composables.values()]
+}
