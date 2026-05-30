0252cc14fb68bf4779488cff95adfebd182abaec
Anton Gorbunov <gorbunov.a@promail366.com>
2026-05-30 15:15:10 +0000

    feat(core): add composable registration

--- packages/core/src/runtime/index.ts
+++ packages/core/src/runtime/index.ts
@@ -1,3 +1,4 @@
 export { initComposableObserver } from './init'
 export { getInstances, registerInstance, unregisterInstance } from './registry'
 export { createInstance } from './create-instance'
+export { registerComposable } from './register-composable'
