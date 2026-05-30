c908cbdec7668409291d04b8d7d5c3351fec0d85
Anton Gor <anton-gor-dev@hotmail.com>
2026-05-30 16:20:25 +0000

    feat(core): add composable lifecycle tracking

--- packages/core/src/runtime/utils/track-composable.ts
+++ packages/core/src/runtime/utils/track-composable.ts
@@ -1,14 +1,15 @@
 import { isDev } from "../../utils"
 import { createInstance } from "./create-instance"
-import { registerDependency, registerInstance, updateInstanceState } from "../registry"
-import { getCurrentComposable, getCurrentOwner, runWithComposable } from "../scope"
+import { registerDependency, registerInstance, unregisterInstance, updateInstanceState } from "../registry"
+import { getCurrentComposable, runWithComposable } from "../scope"
 import { createInstanceId } from "./create-instance-id"
+import { getCurrentScope, onScopeDispose } from "vue"
 
-export function trackComposable<T>(
+export function trackComposable<TArgs extends unknown[], TResult>(
     name: string,
-    fn: () => T,
+    fn: (...args: TArgs) => TResult,
 ) {
-    return () => {
+    return (...args: TArgs) => {
         const id = createInstanceId()
         
         const parentComposableId = getCurrentComposable()
@@ -23,20 +24,35 @@ export function trackComposable<T>(
         const instance = createInstance(
             id,
             name,
-            getCurrentOwner() || null,
         )
 
         registerInstance(instance)
 
         const state = runWithComposable(
             id,
-            fn
+            () => fn(...args),
         )
 
         updateInstanceState(id, state)
 
         if (isDev()) {
             console.log(`Registered composable instance: ${instance.name} (ID: ${instance.id})`)
+        }
+
+        if (getCurrentScope()) {
+            onScopeDispose(() => {
+                if (isDev()) {
+                    console.log(`Unregistering composable instance: ${instance.name} (ID: ${instance.id})`)
+                }
+
+                unregisterInstance(id)
+            })
+        } else {
+            if (isDev()) {
+                console.warn(
+                    `Composable instance "${instance.name}" (ID: ${instance.id}) is not registered to any scope. It will not be automatically unregistered and may lead to memory leaks.`,
+                )
+            }
         }
 
         return {
