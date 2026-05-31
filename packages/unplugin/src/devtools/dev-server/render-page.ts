9862fcea83b1dc437e33013ff91bc6a07df94397
Anton Gorbunov <gorbunov.a@promail366.com>
2026-05-31 07:29:06 +0000

    feat(devtools): scaffold standalone devtools app

---
+++ packages/unplugin/src/devtools/dev-server/render-page.ts
@@ -0,0 +1,10 @@
+export function renderDevtoolsPage() {
+  return `
+    <!DOCTYPE html>
+    <html>
+      <body>
+        <h1>Composable Observer Devtools</h1>
+      </body>
+    </html>
+  `
+}
--- packages/vue/src/plugin.ts
+++ packages/vue/src/plugin.ts
@@ -1,10 +1,11 @@
 import {Plugin} from "vue"
 import { initComposableObserver } from '@goranton/vue-composable-observer-core'
+import { setupComposableObserverDevtools } from "./devtools"
 
 export const ComposableObserverVuePlugin: Plugin = {
     install(app) {
         initComposableObserver()
 
-        // todo
+        setupComposableObserverDevtools(app)
     }
 }
